15e4cf2d4fb1935d005f8317a47ca88dca50d493
abhi8893 <35069768+abhi8893@users.noreply.github.com>
2021-07-24 11:22:55 +0530

    add function to create tensorboard callback in tfutils.callbacks

--- tfutils/callbacks/callbacks.py
+++ tfutils/callbacks/callbacks.py
@@ -1,5 +1,22 @@
 import tensorflow as tf
+import os
+import datetime
+from pathlib import Path
 
+
+def create_tensorboard_callback(experiment, task=None, parent_dir=None):
+
+    if task is None:
+        task = ''
+    
+    if parent_dir is None:
+        parent_dir = ''
+    
+    log_dir = os.path.join(parent_dir, task, experiment, datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
+    Path(log_dir).mkdir(parents=True, exist_ok=True)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=os.path.abspath(log_dir), profile_batch=0)
+    print(f'Saving TensorBoard log files to " {log_dir}"')
+    return tensorboard_callback
 class StopEarly(tf.keras.callbacks.Callback):
 
     def __init__(self, monitor='loss', min_change=0, patience=0):
